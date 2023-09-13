3fdd4e6f868411468e32de3af53c96c0d0643ac6
Matias Rolón <matiasrolon1301@gmail.com>
2023-09-13 18:50:41 -0300

    Test bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
-#include <Arduino.h>
+#include "BluetoothSerial.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+int Led = 5;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  SerialBT.begin("ESP32test");
+  pinMode(Led, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (SerialBT.available()) {
+    char Mensaje = SerialBT.read();
+    if (Mensaje == 'A') {
+      digitalWrite(Led, HIGH);
+      Serial.println("Encender Led");
+    }
+    else if (Mensaje == 'B') {
+      digitalWrite(Led, LOW);
+      Serial.println("Apagar Led");
+    }
+  }
 }
