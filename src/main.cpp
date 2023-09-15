7b2a8635e4923bf68947cd0848a3f7c72b40d9b3
wilvers <juan.ignacio.wilvers@gmail.com>
2023-09-15 11:09:35 -0300

    P.I.D

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,35 @@
 // #define sensor7 = 35
 // #define sensor8 = 34
 
-
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+int proportional = 0;
+int derivative = 0;
+int integral = 0;
+int lastErr = 0;
+
+float kp = 0;
+float ki = 0;
+float kd = 0;
+
+float speed = 0;
+
+int velocity = 100;
+
+float pidLeft = 0;
+float pidRight = 0;
+
+int maxSpeed = 170;
+int minSpeed = 30;
+
+
 BluetoothSerial SerialBT;
 
 QTRSensors qtr;
+
+int setpoint = 3400;
 
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
@@ -26,62 +47,96 @@ int Led = 5;
 
 void calibration()
 {
-    qtr.setTypeAnalog();
-    qtr.setSensorPins((const uint8_t[]){
-      13, 27, 25, 26, 33, 32, 35, 34
-      }, SensorCount);
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){
+                        13, 27, 25, 26, 33, 32, 35, 34},
+                    SensorCount);
 
-    delay(500);
-    pinMode(Led, OUTPUT);
-    digitalWrite(Led, HIGH);
-    for (uint16_t i = 0; i < 1000; i++)
-    {
-        qtr.calibrate();
-    }
-    digitalWrite(Led, LOW); //
+  delay(500);
+  pinMode(Led, OUTPUT);
+  digitalWrite(Led, HIGH);
+  for (uint16_t i = 0; i < 1000; i++)
+  {
+    qtr.calibrate();
+  }
+  digitalWrite(Led, LOW); //
 
-    // print the calibration minimum values measured when emitters were on
+  // print the calibration minimum values measured when emitters were on
 }
 
-void getPosition(){
+int getPosition()
+{
   int position = qtr.readLineWhite(sensorValues);
-      // print the sensor values as numbers from 0 to 1000, where 0 means maximum
-    // reflectance and 1000 means minimum reflectance, followed by the line
-    // position
-    
-    for (uint8_t i = 0; i < SensorCount; i++)
-      {
-        SerialBT.print(sensorValues[i]);
-        SerialBT.print(" || ");
-      }
-      SerialBT.println(position);
-      SerialBT.print('\n');
-
-    
-     delay(250);
+  return position;
 }
 
-void setup() {
+void printPositionAndSensors()
+{
+  int position = qtr.readLineWhite(sensorValues);
+
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    SerialBT.print(sensorValues[i]);
+    SerialBT.print(" || ");
+  }
+  SerialBT.println(position);
+  SerialBT.print('\n');
+
+  delay(250);
+}
+
+void setup()
+{
   Serial.begin(9600);
   SerialBT.begin("ESP32test");
   pinMode(Led, OUTPUT);
   calibration();
 }
 
-void loop() {
-  if (SerialBT.available()) {
+void loop()
+{
+  if (SerialBT.available())
+  {
     char Mensaje = SerialBT.read();
-    if (Mensaje == 'A') {
+    if (Mensaje == 'A')
+    {
       digitalWrite(Led, HIGH);
       Serial.println("Encender Led");
     }
-    else if (Mensaje == 'B') {
+    else if (Mensaje == 'B')
+    {
       digitalWrite(Led, LOW);
       Serial.println("Apagar Led");
     }
   }
 
-  getPosition();
+  int position = getPosition();
 
+  proportional = position - setpoint;
 
+  derivative = proportional - lastErr;
+
+  integral = integral + proportional;
+
+  speed = (proportional * kp) + (derivative * kd) + (integral * ki);
+
+  lastErr = proportional;
+
+  pidLeft = (velocity + speed);
+  pidRight = (velocity - speed);
+
+  if (pidLeft > maxSpeed) pidLeft = maxSpeed;
+  else if (pidLeft < minSpeed) pidLeft = minSpeed;
+
+  if (pidRight < minSpeed) pidRight = minSpeed;
+  else if (pidRight > maxSpeed) pidRight = maxSpeed;
+
+  SerialBT.println('motor left');
+  SerialBT.println(pidLeft);
+  SerialBT.println('motor right');
+  SerialBT.println(pidRight);
 }
